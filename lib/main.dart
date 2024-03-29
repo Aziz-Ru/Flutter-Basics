1eb62165afd89f387dff3438b7dd4472685ae819
silicon <siliconoxide4@gmail.com>
2024-03-29 22:46:01 +0600

    update image

--- lib/main.dart
+++ lib/main.dart
@@ -8,24 +8,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Flutter",
-        home: Scaffold(
-          backgroundColor: Colors.brown,
-          appBar: AppBar(
-            backgroundColor: Colors.redAccent,
-            title: const Text(
-              'App',
-              textDirection: TextDirection.ltr,
-            ),
+      title: "Flutter",
+      home: Scaffold(
+        backgroundColor: Colors.brown,
+        appBar: AppBar(
+          backgroundColor: Colors.redAccent,
+          title: const Text(
+            'App',
+            textDirection: TextDirection.ltr,
           ),
-          body: const Center(
-            child: Text('My App',
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                    decoration: TextDecoration.none,
-                    color: Colors.blue,
-                    fontSize: 30.0)),
-          ),
-        ));
+        ),
+        // body: const Image(
+        //   image: AssetImage('images/bag-1.png'),
+        //   fit: BoxFit.cover,
+        // )));
+
+        body: const Image(
+          image: NetworkImage(
+              'https://unsplash.com/photos/a-person-swimming-in-the-ocean-with-a-camera-NhWxAIs61MM'),
+        ),
+      ),
+    );
   }
 }
