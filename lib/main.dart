91ba2de7443c655b601d30a48714380ba10bb151
silicon <siliconoxide4@gmail.com>
2024-03-29 20:14:28 +0600

    Container update

--- lib/main.dart
+++ lib/main.dart
@@ -7,35 +7,40 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      textDirection: TextDirection.ltr,
-      children: [
-        Text(
-          'Hello',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(fontSize: 30.0),
-        ),
-        Text(
-          'World',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(fontSize: 30.0),
-        ),
-        Text(
-          'Flutter',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(fontSize: 30.0),
-        ),
-        Text(
-          'Framework',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(fontSize: 30.0),
-        ),
-        Text(
-          'Framework',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(fontSize: 30.0),
-        )
-      ],
+    return Container(
+      // padding: EdgeInsets.symmetric(vertical: 100.0),
+      padding: EdgeInsets.all(50.0),
+      decoration: BoxDecoration(color: Colors.blue),
+      child: const Column(
+        textDirection: TextDirection.ltr,
+        children: [
+          Text(
+            'Hello',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 130.0),
+          ),
+          Text(
+            'World',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 130.0),
+          ),
+          Text(
+            'Flutter',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 30.0),
+          ),
+          Text(
+            'Framework',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 130.0),
+          ),
+          Text(
+            'Framework',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 130.0),
+          )
+        ],
+      ),
     );
   }
 }
