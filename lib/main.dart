a26634bfdae065597c9df05f4e65619963764a71
silicon <siliconoxide4@gmail.com>
2024-03-29 19:58:05 +0600

    colum update

--- lib/main.dart
+++ lib/main.dart
@@ -7,28 +7,24 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return const Column(
       textDirection: TextDirection.ltr,
       children: [
-        Expanded(
-            //Expanded use krle autoamtic feet hbe
-            child: Text(
+        Text(
           'Hello',
           textDirection: TextDirection.ltr,
           style: TextStyle(fontSize: 30.0),
-        )),
-        Expanded(
-            child: Text(
+        ),
+        Text(
           'World',
           textDirection: TextDirection.ltr,
           style: TextStyle(fontSize: 30.0),
-        )),
-        Expanded(
-            child: Text(
+        ),
+        Text(
           'Flutter',
           textDirection: TextDirection.ltr,
           style: TextStyle(fontSize: 30.0),
-        )),
+        ),
         Text(
           'Framework',
           textDirection: TextDirection.ltr,
