097d63055c5cedcbf68539445a762b65401ffb86
silicon <siliconoxide4@gmail.com>
2024-03-30 09:52:59 +0600

    Listtile update

--- lib/main.dart
+++ lib/main.dart
@@ -9,26 +9,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.cyanAccent,
-          title: const Text('CSE'),
-        ),
-        body: const Stack(
-          children: [
-            //Overfollow hosse
-            Image(image: AssetImage('images/bag-1.png')),
-            Image(image: AssetImage('images/bag-2.png')),
-            Image(image: AssetImage('images/bag-3.png')),
-            Image(image: AssetImage('images/bag-4.jpeg')),
-            Image(image: AssetImage('images/bag-5.jpeg')),
-            Image(image: AssetImage('images/bag-6.jpeg')),
-            Image(image: AssetImage('images/bag-7.jpeg')),
-            Image(image: AssetImage('images/bag-8.jpeg')),
-            Image(image: AssetImage('images/bag-9.jpeg')),
-            Image(image: AssetImage('images/bag-10.jpeg')),
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            backgroundColor: Colors.cyanAccent,
+            title: const Text('CSE'),
+          ),
+          body: ListView(
+            children: [
+              ListTile(
+                leading: Image.asset('images/bag-1.png'),
+                title: const Text('Red Bag'),
+                subtitle: const Text('This subtitle'),
+                trailing: const Icon(Icons.delete),
+              ),
+              ListTile(
+                leading: Image.asset('images/bag-2.png'),
+                title: const Text('Blue Bag'),
+                subtitle: const Text('This subtitle'),
+                trailing: const Icon(Icons.delete),
+              ),
+              ListTile(
+                leading: Image.asset('images/bag-3.png'),
+                title: const Text('Black Bag'),
+                subtitle: const Text('This subtitle'),
+                trailing: const Icon(Icons.delete),
+              ),
+            ],
+          )),
     );
   }
 }
