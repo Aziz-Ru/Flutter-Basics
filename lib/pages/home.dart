58111e0b2d4bd881e7e5e8f8e92d33e0738b1fe9
silicon <siliconoxide4@gmail.com>
2024-03-30 11:29:30 +0600

    Button update

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,23 +4,32 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Center(
-        child: ListView(
-      padding: const EdgeInsets.all(30),
+    return Column(
       children: [
-        Card(
-          color: Colors.cyan,
-          child: Image.asset('images/bag-1.png'),
-        ),
-        Card(
-          color: Colors.pink,
-          child: Image.asset('images/bag-3.png'),
-        ),
-        Card(
-          color: Colors.cyanAccent,
-          child: Image.asset('images/bag-2.png'),
+        TextButton(
+            onPressed: () {
+              // print('Clicked Me');
+            },
+            style: TextButton.styleFrom(
+                backgroundColor: Colors.black,
+                textStyle:
+                    const TextStyle(color: Colors.white, fontSize: 18.0)),
+            child: const Text('Click Me')),
+        ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.purple,
+            ),
+            child: const Text(
+              'Button',
+              style: TextStyle(color: Colors.black),
+            )),
+        FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.pink,
+          child: const Text('Action'),
         )
       ],
-    ));
+    );
   }
 }
