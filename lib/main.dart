3e0884fd4e94ee7f40e8e57689258b24762c36af
silicon <siliconoxide4@gmail.com>
2024-03-30 09:34:23 +0600

    Grid update

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.cyanAccent,
           title: const Text('CSE'),
         ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/pasta.jpg'),
-              const Text('Foods')
-            ],
-          ),
+        body: GridView.extent(
+          maxCrossAxisExtent: 200,
+          mainAxisSpacing: 100,
+          crossAxisSpacing: 100,
+          children: const [
+            Image(image: AssetImage('images/bag-1.png')),
+            Image(image: AssetImage('images/bag-2.png')),
+            Image(image: AssetImage('images/bag-3.png')),
+            Image(image: AssetImage('images/bag-4.jpeg')),
+            Image(image: AssetImage('images/bag-5.jpeg')),
+            Image(image: AssetImage('images/bag-6.jpeg')),
+            Image(image: AssetImage('images/bag-7.jpeg')),
+            Image(image: AssetImage('images/bag-8.jpeg')),
+            Image(image: AssetImage('images/bag-9.jpeg')),
+            Image(image: AssetImage('images/bag-10.jpeg')),
+          ],
         ),
       ),
     );
