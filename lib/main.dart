fde3d2b36798afc50563171b358a39aa5b060311
Aziz <siliconoxide4@gmail.com>
2024-04-26 18:08:57 +0600

    Update main

--- lib/main.dart
+++ lib/main.dart
@@ -10,24 +10,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Flutter",
       home: Scaffold(
-        backgroundColor: Colors.brown,
-        appBar: AppBar(
-          backgroundColor: Colors.redAccent,
-          title: const Text(
-            'App',
-            textDirection: TextDirection.ltr,
+          backgroundColor: Colors.brown,
+          appBar: AppBar(
+            backgroundColor: Colors.redAccent,
+            title: const Text(
+              'First Mobile Application',
+              textDirection: TextDirection.ltr,
+            ),
           ),
-        ),
-        // body: const Image(
-        //   image: AssetImage('images/bag-1.png'),
-        //   fit: BoxFit.cover,
-        // )));
-
-        body: const Image(
-          image: NetworkImage(
-              'https://unsplash.com/photos/a-person-swimming-in-the-ocean-with-a-camera-NhWxAIs61MM'),
-        ),
-      ),
+          body: const Center(
+            child: Text('Hello World'),
+          )),
     );
   }
 }
