6dcd7d38b66b23676ec95b5b7a836e49d51ad650
Aziz <siliconoxide4@gmail.com>
2024-07-06 15:00:12 +0600

    Added AnimatedContainer

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutterfirst/widget/Column/columnwidget.dart';
 
@@ -8,18 +10,54 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "Flutter",
-        home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.redAccent,
-              title: const Text(
-                'First Mobile Application',
-                textDirection: TextDirection.ltr,
-              ),
-            ),
-            body: const SafeArea(
-              child: MyColumnWidget(),
-            )));
+    return const MaterialApp(title: "Flutter", home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double _width = 100;
+  double _height = 100;
+  Color _color = Colors.red;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: const Text(
+          'First Mobile Application',
+          textDirection: TextDirection.ltr,
+        ),
+      ),
+      body: SafeArea(
+        child: MyColumnWidget(
+          animateWidth: _width,
+          animateHeight: _height,
+          animateColor: _color,
+          animateBorderRadius: _borderRadius,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _width = Random().nextInt(400).toDouble();
+            _height = Random().nextInt(400).toDouble();
+            _color = Color.fromRGBO(Random().nextInt(255),
+                Random().nextInt(255), Random().nextInt(255), 1);
+            _borderRadius =
+                BorderRadius.circular(Random().nextInt(50).toDouble());
+            // print(_height);
+          });
+        },
+        child: const Icon(Icons.refresh),
+      ),
+    );
   }
 }
