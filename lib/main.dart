df21f34b5669ef17e1398ec4447e03fab77499c0
Aziz <siliconoxide4@gmail.com>
2024-07-06 15:42:05 +0600

    Added PageView

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutterfirst/widget/Column/columnwidget.dart';
-import 'package:flutterfirst/widget/bottomNavigation/bottomNavigationBar.dart';
+import 'package:flutterfirst/widget/PageView/pageview.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +11,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: "Flutter", home: MyBottomNavigationBar());
+    return const MaterialApp(title: "Flutter", home: MyPageView());
   }
 }
 
